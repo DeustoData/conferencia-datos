20e75e33f6903cfb2de15d53c1433f26383dbb23
Guille <ggalmazor@gmail.com>
2018-03-22 11:16:15 +0100

    Fix contact form submission target to send results to a Google Spreadsheet

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,70 +1,58 @@
-$(function() {
+$(function () {
 
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function ($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function ($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var $contactForm = $('#contactForm');
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var $success = $('#success');
+      $.ajax({
+        type: "POST",
+        url: "//docs.google.com/forms/d/e/1FAIpQLSdpg5lzboiuxs-82Zm53f_45g2sbgQZBOIpGwNGoF3VR5pr1Q/formResponse",
+        dataType: "json",
+        data: {
+          'entry.1398314030': name,
+          'entry.1922591094': email,
+          'entry.1658098952': phone,
+          'entry.2115776476': message
         },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+        statusCode: {
+          200: function () {
+            // Success message
+            $success.html("<div class='alert alert-success'>");
+            $success.find('> .alert-success')
+                .html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>")
+                .append("<strong>Your message has been sent. </strong>")
+                .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+            //clear all fields
+            $contactForm.trigger("reset");
+          }
+        }
+      });
+    },
+    filter: function () {
+      return $(this).is(":visible");
+    },
+  });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+  $("a[data-toggle=\"tab\"]").click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
 });
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
+$('#name').focus(function () {
+  $('#success').html('');
 });
